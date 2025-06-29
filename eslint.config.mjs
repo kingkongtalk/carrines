b60dba83ce9fa85815e04a69d5821bcd4e96aa57
kingkong <kingkongtalk@163.com>
2025-06-29 15:09:14 +0800

    chore: 更新 wrangler 配置、ESLint 配置和 Worker ���������������������������������������

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,8 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@next/next/no-img-element": "off",
       "jsx-a11y/alt-text": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "import/no-anonymous-default-export": "off",
     },
   },
 ];
